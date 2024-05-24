73c7f76d6327906fe30a4a05648ac00083fc5614
Manar Elsaid <elsaidmanar@gmail.com>
2024-05-24 23:25:20 +0300

    task 1 and 2

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
+import Notifications from './Notifications.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <div className='root-notifications'>
+      <Notifications/>
+    </div>
   </React.StrictMode>
 );
 
